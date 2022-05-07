4313dde9154bc3432f2c020c11202d9d0423cee0
kostyashelest <skostya88@gmail.com>
2022-05-07 18:06:40 +0300

    User administrator seeder and update readme

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'email' => 'admin@site.com',
+            'name' => 'admin',
+            'password' => Hash::make('password')
+        ]);
+        $user->assignRole(UserRole::ADMIN);
+    }
+}
