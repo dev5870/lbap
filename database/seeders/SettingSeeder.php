bab62239af8fabfab2409c373233f0f06d1d0c95
kostyashelest <skostya88@gmail.com>
2022-05-09 10:24:16 +0300

    Model, migration and seeder for site settings

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Setting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class SettingSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            SettingSeeder::class,
-            UserRoleSeeder::class,
-            UserAdminSeeder::class,
+        Setting::create([
+            'site_name' => 'Site name',
         ]);
     }
 }
