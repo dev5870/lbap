b780c7c62bdbbbea885abdc983cbf9d92f8aba92
kostyashelest <skostya88@gmail.com>
2022-06-08 11:11:29 +0300

    User params

---
+++ app/Models/UserParam.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\UserParam
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $user_uuid
+ * @property string|null $username
+ * @property string|null $description
+ * @property int|null $mfa
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereMfa($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereUserUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserParam whereUsername($value)
+ * @mixin \Eloquent
+ */
+class UserParam extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'user_uuid',
+        'username',
+        'description',
+        'mfa',
+    ];
+}
---
+++ database/migrations/2022_06_08_110317_create_user_params_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_params', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->uuid('user_uuid')->nullable();
+            $table->string('username', 15)->nullable();
+            $table->string('description', 300)->nullable();
+            $table->boolean('mfa')->default(false)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_params');
+    }
+};
